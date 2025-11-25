e7645488caa8a330cedf45e1593686edd83bed3b
chris bridges <christopher.bridges1@snhu.edu>
2025-11-25 18:43:51 -0500

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
+
+require('./app_api/models/db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -26,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
